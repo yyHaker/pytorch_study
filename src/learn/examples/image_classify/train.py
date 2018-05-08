fc112399d61eead2161a356243fa6e32678c199e
yyhaker <572176750@qq.com>
2018-05-07 20:52:35 -0700

    add GPU config

--- src/learn/examples/image_classify/train.py
+++ src/learn/examples/image_classify/train.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
-
+import torch.utils.data as Data
 import torchvision
 import torchvision.transforms as transforms
 
@@ -14,20 +14,19 @@ import numpy as np
 from models import LeNet5
 
 USE_CUDA = torch.cuda.is_available()
-device = torch.device("cuda: 0" if USE_CUDA else "cpu")
 
 # load data
 transform = transforms.Compose([transforms.ToTensor(), transforms.
                                Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
 trainset = torchvision.datasets.CIFAR10(root='./cifiar', train=True,
                                         transform=transform, download=True)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+trainloader = Data.DataLoader(trainset, batch_size=4,
+                              shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./cifiar', train=False,
                                        transform=transform, download=True)
-testLoader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+testLoader = Data.DataLoader(testset, batch_size=4,
+                             shuffle=False, num_workers=2)
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse',
            'ship', 'truck')
 
@@ -49,7 +48,7 @@ print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 # define models
 net = LeNet5()
 if USE_CUDA:
-    net.to(device)
+    net = net.cuda()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
 
@@ -58,8 +57,9 @@ for epoch in range(100):
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
+        inputs, labels = Variable(inputs), Variable(labels)
         if USE_CUDA:
-            inputs, labels = inputs.to(device), labels.to(device)
+            inputs, labels = inputs.cuda(), labels.cuda()
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, labels)
@@ -67,20 +67,26 @@ for epoch in range(100):
         optimizer.step()
 
         # print statistics
-        running_loss += loss.item()
+        running_loss += loss.data[0]
         if i % 2000 == 1999:  # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss / 2000))
             running_loss = 0.0
     # every epoch test the model
     correct = 0
     total = 0
-    with torch.no_grad():
-        for data in testLoader:
-            images, labels = data
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+    for data in testLoader:
+        images, labels = data
+        images = Variable(images)
+        if USE_CUDA:
+            images, labels = images.cuda(), labels.cuda()
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        if USE_CUDA:
+            predicted = predicted.cpu().data.numpy()
+        else:
+            predicted = predicted.numpy()
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
     print("epoch %d , Accuracy of the net on the 1000 test images: %d %% "
           % (epoch, 100 * correct / total))
 
